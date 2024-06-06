f4f8caa8ce63a3df9f9e589a0d2f5d6362b26dfc
Alok Maheshwari <alok.maheshwari@hcl.com>
2024-06-06 18:12:15 +0530

    exception update

--- .github/workflows/query.ps1
+++ .github/workflows/query.ps1
@@ -4,9 +4,24 @@ $Runtime = (get-date).AddHours(3)
 Set-AzContext -SubscriptionObject "679f3d56-bed2-429f-9e31-4d7bf67e14c7"
 
 while ($Time -eq $Runtime) {
-    Get-AzResourceGroup -Name Alok_Maheshwari_RG 
-    $Time.AddMinutes(1)
+try {
+    Get-AzResourceGroup -Name Alok_Maheshwari_RG -ErrorAction Stop    
+    $Time.AddMinutes(4)
+    $currentTime = $Time.AddMinutes(4)
     sleep 50    
+    
+}
+catch {
+    Write-Output "Activity Started at $($Time) and Exception Occurred at $($currentTime) following was the exception
+    
+    $_.Exception
+    
+    "
+}
+
+
+
+
 }
 
 
