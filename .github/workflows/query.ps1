bcf5fc21af44f830ca78a1d70027ccb438fd2714
Alok Maheshwari <alok.maheshwari@hcl.com>
2024-06-06 17:44:39 +0530

    context set

--- .github/workflows/query.ps1
+++ .github/workflows/query.ps1
@@ -1,6 +1,8 @@
 $Time = Get-Date
 $Runtime = (get-date).AddHours(3)
 
+Set-AzContext -SubscriptionObject "679f3d56-bed2-429f-9e31-4d7bf67e14c7"
+
 while ($Time -eq $Runtime) {
     Get-AzResourceGroup -Name Alok_Maheshwari_RG 
     $Time.AddMinutes(5)
