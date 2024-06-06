b9614db671c4ec64e5577dc9b541979592c67b9a
Alok Maheshwari <alok.maheshwari@hcl.com>
2024-06-06 18:47:19 +0530

    updated time logix

--- .github/workflows/query.ps1
+++ .github/workflows/query.ps1
@@ -3,7 +3,7 @@ $Runtime = (get-date).AddHours(3)
 
 Set-AzContext -Subscription "679f3d56-bed2-429f-9e31-4d7bf67e14c7"
 
-while ($Time -eq $Runtime) {
+while ($Time -ne $Runtime) {
 try {
     Get-AzResourceGroup -Name Alok_Maheshwari_RG -ErrorAction Stop    
     $Time.AddMinutes(4)
